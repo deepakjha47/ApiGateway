5c151b1d5c63b5a1b1597c835f0f50f1f0412326
Deepak Kumar <djha93486@gmail.com>
2024-10-13 13:11:37 +0530

    initial commit

---
+++ src/main/java/com/example/ApiGateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
